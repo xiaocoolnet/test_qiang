79b2fd119a79f569cae2cd32bbcba0d3d8b7554c
zhangsiqiang07 <1328254616@qq.com>
2016-08-13 11:09:37 +0800

    zsq

--- app/src/main/java/cn/xiaocool/test_qiang/MainActivity.java
+++ app/src/main/java/cn/xiaocool/test_qiang/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private String aa;
+    private  int b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
